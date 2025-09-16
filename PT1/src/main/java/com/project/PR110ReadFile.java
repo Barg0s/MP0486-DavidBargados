28316ca24980774f388040bd33dbfa693e031c4c
Barg0s <150375261+Barg0s@users.noreply.github.com>
2025-09-16 19:21:28 +0200

    ex0 + mitad 1

---
+++ PT1/src/main/java/com/project/PR110ReadFile.java
@@ -0,0 +1,33 @@
+package com.project;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class PR110ReadFile {
+
+    public static void main(String[] args) {
+        String camiFitxer = System.getProperty("user.dir") + "/data/GestioTasques.java";
+        llegirIMostrarFitxer(camiFitxer);  // Només cridem a la funció amb la ruta del fitxer
+    }
+
+    // Funció que llegeix el fitxer i mostra les línies amb numeració
+    public static void llegirIMostrarFitxer(String camiFitxer) {
+        try {
+            List<String> linies = Files.readAllLines(Paths.get(camiFitxer), StandardCharsets.UTF_8);
+            mostrarLinies(linies);
+        } catch (IOException e) {
+            System.out.println("Error en la lectura del fitxer: " + camiFitxer);
+            e.printStackTrace();
+        }
+    }
+
+    public static void mostrarLinies(List<String> l){
+        System.out.println("Fitxer GestioTasques:");
+        for (int i = 0; i < l.size();i++){
+            System.out.println(i + ":" + l.get(i));
+        }
+    }
+}
---
+++ PT1/src/main/java/com/project/PR111Files.java
@@ -0,0 +1,25 @@
+package com.project;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class PR111Files {
+
+    public static void main(String[] args) {
+        String camiFitxer = System.getProperty("user.dir") + "/data/pr111";
+        gestionarArxius(camiFitxer);
+    }
+
+    public static void gestionarArxius(String camiFitxer) {
+        try {
+            Files.createDirectories(Path.of(camiFitxer));
+            System.out.println("carpeta: creada a" + camiFitxer);
+
+        } catch (IOException e) {
+            System.out.println("Error en la creació de la carpeta: " + camiFitxer);
+            e.printStackTrace();
+        }
+    }
+
+}
