b0aedd8748bec0f37b25130d830b3a2fdaec7dba
Barg0s <150375261+Barg0s@users.noreply.github.com>
2025-12-16 19:23:38 +0100

    .

---
+++ RA2/PT2.3/src/main/java/com/project/domain/Autor.java
@@ -0,0 +1,59 @@
+package com.project.domain;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+// TODO 1: Afegir anotacions @Entity i @Table
+@Entity
+@Table(name = "Autor")
+public class Autor implements Serializable {
+
+
+    private static final long serialVersionUID = 1L;
+
+    // TODO 2: Afegir @Id i @GeneratedValue
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="autor_id", unique=true, nullable=false)
+    private Long autorId;
+
+    private String nom;
+
+    // TODO 3: Relació ManyToMany. 
+    // PISTA: L'enunciat diu que Autor és la part inversa ("mappedBy").
+    // Això vol dir que la taula intermèdia la gestiona l'entitat 'Llibre'.
+    
+    @ManyToMany(
+        mappedBy= "autors",
+        fetch = FetchType.LAZY  // CANVIAT: de EAGER a LAZY
+    )
+
+    private Set<Llibre> llibres = new HashSet<>();
+
+    public Autor() {}
+
+    public Autor(String nom) {
+        this.nom = nom;
+    }
+
+    public Long getAutorId() { return autorId; }
+    public void setAutorId(Long autorId) { this.autorId = autorId; }
+    public String getNom() { return nom; }
+    public void setNom(String nom) { this.nom = nom; }
+    public Set<Llibre> getLlibres() { return llibres; }
+    public void setLlibres(Set<Llibre> llibres) { this.llibres = llibres; }
+
+    @Override
+    public String toString() {
+        return "Autor{id=" + autorId + ", nom='" + nom + "'}";
+    }
+}
---
+++ RA2/PT2.3/src/main/java/com/project/domain/Llibre.java
@@ -0,0 +1,88 @@
+package com.project.domain;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+// TODO 1: Afegir anotacions @Entity i @Table
+
+@Entity
+@Table(name = "Llibre")
+public class Llibre implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="llibre_id", unique=true, nullable=false)
+    private Long llibreId;
+
+    private String isbn;
+    private String titol;
+    private String editorial;
+    private Integer anyPublicacio;
+
+    // TODO 3: Relació ManyToMany amb Autor.
+    // PISTA: Aquesta entitat és la "propietària" de la relació. 
+    // Cal definir aquí el @JoinTable explícitament.
+    // PISTA EXTRA: Fes servir fetch = FetchType.LAZY per eficiència.
+    @ManyToMany(
+        cascade = {CascadeType.PERSIST, CascadeType.MERGE},  // MAI CascadeType.REMOVE!
+        fetch = FetchType.LAZY  // CANVIAT: de EAGER a LAZY
+    )
+    @JoinTable(
+        name = "llibre_autor",
+        joinColumns=@JoinColumn(name = "llibreId"),
+        inverseJoinColumns= @JoinColumn(name= "autorId")
+    )    
+    private Set<Autor> autors = new HashSet<>();
+    // TODO 4: Relació OneToMany amb Exemplar.
+    // PISTA: mappedBy = "llibre"
+
+    @OneToMany(mappedBy = "llibre", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<Exemplar> exemplars;
+
+    public Llibre() {this.exemplars = new HashSet<>();
+}
+
+    public Llibre(String isbn, String titol, String editorial, Integer anyPublicacio) {
+        this.exemplars = new HashSet<>();
+        this.isbn = isbn;
+        this.titol = titol;
+        this.editorial = editorial;
+        this.anyPublicacio = anyPublicacio;
+    }
+
+    public Long getLlibreId() { return llibreId; }
+    public void setLlibreId(Long llibreId) { this.llibreId = llibreId; }
+    public String getIsbn() { return isbn; }
+    public void setIsbn(String isbn) { this.isbn = isbn; }
+    public String getTitol() { return titol; }
+    public void setTitol(String titol) { this.titol = titol; }
+    public String getEditorial() { return editorial; }
+    public void setEditorial(String editorial) { this.editorial = editorial; }
+    public Integer getAnyPublicacio() { return anyPublicacio; }
+    public void setAnyPublicacio(Integer anyPublicacio) { this.anyPublicacio = anyPublicacio; }
+    public Set<Autor> getAutors() { return autors; }
+    public void setAutors(Set<Autor> autors) { this.autors = autors; }
+    public Set<Exemplar> getExemplars() { return exemplars; }
+    public void setExemplars(Set<Exemplar> exemplars) { this.exemplars = exemplars; }
+
+    @Override
+    public String toString() {
+        // PISTA: No imprimim les llistes (autors/exemplars) per evitar bucles infinits
+        return "Llibre{id=" + llibreId + ", isbn='" + isbn + "', titol='" + titol + "'}";
+    }
+}
