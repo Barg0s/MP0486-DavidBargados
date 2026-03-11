5b7d39f02435b0ac03253da978a9ee135ebe81f9
Barg0s <150375261+Barg0s@users.noreply.github.com>
2026-03-11 18:26:10 +0100

    .

--- RA3/PT3.1/exemple-mongodb/src/exercici1.js
+++ RA3/PT3.1/exemple-mongodb/src/exercici1.js
@@ -29,8 +29,67 @@ async function parseXMLFile(filePath) {
   }
 }
 
-// TODO
 
-function processCoffee(data){
+function processCoffeeData(data) {
+  const coffees = Array.isArray(data.posts.row) ? data.posts.row : [data.posts.row];
 
+  return coffees.map(row => ({
+    question: {
+      Id: row.Id,
+      PostTypeId: row.PostTypeId,
+      AcceptedAnswerId: row.AcceptedAnswerId,
+      CreationDate: row.CreationDate,
+      Score: row.Score,
+      ViewCount: row.ViewCount,
+      Body: row.Body,
+      OwnerUserId: row.OwnerUserId || null,
+      LastActivityDate: row.LastActivityDate ,
+      Title: row.Title,
+      Tags: row.Tags,
+      AnswerCount: row.AnswerCount ,
+      CommentCount: row.CommentCount,
+      ContentLicense: row.ContentLicense
+    }
+  }));
 }
+
+
+
+async function loadDataToMongoDB() {
+  const uri = process.env.MONGODB_URI || 'mongodb://root:password@localhost:27017/';
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+    console.log('Conectado a MongoDB');
+
+    const database = client.db('coffee_db');
+    const collection = database.collection('coffee');
+
+    console.log('Llegint el fitxer XML...');
+    const xmlData = await parseXMLFile(xmlFilePath);
+
+    console.log('Processant les dades...');
+    let questions = processCoffeeData(xmlData);
+
+    console.log('Eliminant dades existents...');
+    await collection.deleteMany({});
+ 
+    //TODO -> ORDENAR las 10000 preguntas con mas viewCount
+    
+    console.log('Inserint dades a MongoDB...');
+    const result = await collection.insertMany(questions);
+
+    console.log(`${result.insertedCount} documents inserits correctament.`);
+    console.log('Dades carregades amb èxit!');
+
+
+  } catch (error) {
+    console.error('Error carregant les dades a MongoDB:', error);
+  } finally {
+    await client.close();
+    console.log('Connexió a MongoDB tancada');
+  }
+}
+
+loadDataToMongoDB();
---
+++ RA3/PT3.1/exemple-mongodb/src/exercici2.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const path = require('path');
+const { MongoClient } = require('mongodb');
+const xml2js = require('xml2js');
+require('dotenv').config();
